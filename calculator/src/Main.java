695e327d408b0fed5dc3a360803a988bb288b8ea
ms <soe3676@nate.com>
2022-09-30 15:14:27 +0900

    calculator Fix

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -1,18 +1,1 @@
 public class Main {
-    public static void main(String[] args){
-
-        Count result = new Count();
-
-        result.setA(3.0);
-        result.setB(17);
-
-        System.out.println(result.getA() + " + " + result.getB()
-                + " = " + result.getPlus());
-        System.out.println(result.getA() + " - " + result.getB()
-                + " = " + result.getSubtract());
-        System.out.println(result.getA() + " * " + result.getB()
-                + " = " + result.getMultiply());
-        System.out.println(result.getA() + " / " + result.getB()
-                + " = " + result.getDivide());
-    }
-}
