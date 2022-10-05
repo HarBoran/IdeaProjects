d07e8ea421268cdc9ce7e12319e868ebc2632565
ms <soe3676@nate.com>
2022-10-05 14:38:19 +0900

    beginner

---
+++ P253Polymorphism/src/Ex7_8.java
@@ -0,0 +1,63 @@
+class Product {
+    int price;
+    int bonusPoint;
+
+    Product(int price){
+        this.price = price;
+        bonusPoint = (int)(price/10);
+    }
+}
+
+class Tv1 extends Product{
+    Tv1(){
+        super(100);
+    }
+    public String toString() {
+        return "TV";
+    }
+}
+
+class Computer extends Product{
+    Computer() {
+        super(200);
+    }
+    public String toString(){
+        return "Computer";
+    }
+}
+
+class Buyer{
+    int money = 1000;
+    int bonusPoint = 0;
+
+    void buy(Product p){
+        if(money < p.price){
+            System.out.println("잔액이 부족합니다");
+            return;
+        }
+        money -=p.price;
+        bonusPoint += p.bonusPoint;
+        System.out.println(p + "을/를 구매하셨습니다.");
+    }
+}
+
+
+public class Ex7_8 {
+    public static void main(String[] args){
+        Buyer b = new Buyer();
+
+
+        Tv1 t = new Tv1();
+        b.buy(t);
+//      b.buy(t.Tv1());  //안됨
+//      b.buy(Tv1());  //안됨
+
+        b.buy(new Tv1());
+        b.buy(new Computer());
+
+
+
+        System.out.println("현제 남은 돈은" + b.money + "만원입니다");
+        System.out.println("현제 남은 보너스 점수는" + b.bonusPoint + "점입니다.");
+    }
+}
